d56a4ed4eb8f2495d960bc33ba6728c2e42188a0
Nolen Lawton <mnlawton@icloud.com>
2023-01-19 18:49:13 -0600

    slayed the beast and disabled weapons without the necessary AP

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
-
 }
 
 let AP = 100
 let HP = 100
+let fungusAnimation = 'walk'
 
 function attack() {
     if($(this).hasClass('arcane-sceptre')){
@@ -44,17 +44,48 @@ function attack() {
 
     if(HP < 0) {
         HP = 0
+        fungusAnimation = 'dead';
     }
 
     if(AP < 0) {
         AP = 0
+        fungusAnimation = 'jump'
     }
 
     console.log(`HP: ${HP}, AP: ${AP}`)
-
     render()
 }
 
 function render() {
-    $()
+    
+    if(AP < 38){
+        $('.dragon-blade').prop('disabled', true)
+        console.log('Dragon Blade offline')
+    }
+    if(AP < 33){
+        $('.star-fire').prop('disabled', true)
+        console.log('Star Fire offline')
+    }
+    if(AP < 23){
+        $('.entangle').prop('disabled', true)
+        console.log('Entangle offline')
+    }
+    if(AP < 12){
+        $('.arcane-sceptre').prop('disabled', true)
+        console.log('Arcane Sceptor offline')
+    }
+
+    $(`.ap-text`).text(AP + ' AP')
+    $(`.hp-text`).text(HP + ' HP')
+    $(`#ap-meter`).val(AP)
+    $(`#hp-meter`).val(HP)
+
+    if(HP === 0) {
+        $(`.walk`).attr(`class`, `freaky-fungus ${fungusAnimation}`)
+    }
+
+    if(AP === 0) {
+        $(`.walk`).attr(`class`, `freaky-fungus ${fungusAnimation}`)
+    }
+
 }
